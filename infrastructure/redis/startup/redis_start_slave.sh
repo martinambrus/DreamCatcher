d2ab626026069918e7f6456f7d0e233c5eda376e
Martin Ambruš <martin@martinambrus.com>
2023-08-29 08:39:51 +0200

    chore: REDIS replication

---
+++ infrastructure/redis/startup/redis_start_slave.sh
@@ -0,0 +1,12 @@
+# start server in background and wait for 1 sec
+redis-server --daemonize yes && sleep 1
+# slurp all data from file to redis in memory db (note the dir)
+redis-cli < /usr/src/redis_startup/error_codes.redis
+# persist data to disk
+redis-cli save
+# stop background server
+redis-cli shutdown
+# start the server normally
+redis-server
+# mark slave server as a replica
+redis-cli < /usr/src/redis_startup/redis_slave.redis
