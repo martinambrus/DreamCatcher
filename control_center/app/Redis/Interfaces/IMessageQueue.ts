099e173b54aeb740aadac7c9f10755a37c157a7e
Martin Ambruš <martin@martinambrus.com>
2023-08-31 11:52:54 +0200

    Squash 15 commits
    * e68e63b by Martin Ambruš <martin@martinambrus.com>
    Squash 14 commits
    * 52d619c by Martin Ambruš <martin@martinambrus.com>
    chore: remove subtree
    * 0f28335 by Martin Ambruš <martin@martinambrus.com>
    Merge commit 'ea16da47005e2b851357da0ef3fafe06c33c0cd8'
    * ea16da4 by Martin Ambruš <martin@martinambrus.com>
    Squashed 'Interfaces/' changes from 0f4f11e..d0673cf
    * b6def9c by Martin Ambruš <martin@martinambrus.com>
    Merge commit '02886aa545d7ec22c9987cc93fcc8b5d5fca8488' as 'Interfaces'
    * 02886aa by Martin Ambruš <martin@martinambrus.com>
    Squashed 'Interfaces/' content from commit 0f4f11e
    * a9aeb0c by Martin Ambruš <martin@martinambrus.com>
    chore: remove submodules
    * 9f66b9e by Martin Ambruš <martin@martinambrus.com>
    chore: submodule update
    * d30ff26 by Martin Ambruš <martin@martinambrus.com>
    wip
    * 4531a90 by Martin Ambruš <martin@martinambrus.com>
    chore: PHPStorm IDE settings
    and 5 more

---
+++ control_center/app/Redis/Interfaces/IMessageQueue.ts
@@ -0,0 +1,18 @@
+/**
+ * Interface for a message queue broker.
+ */
+export interface IMessageQueue {
+
+  /**
+   * Sends message to the message queue.
+   *
+   * @param { string } topic    Topic to sent the message to.
+   * @param { Object } msg      Message data to publish. Will be converted into a JSON string.
+   * @param { string } trace_id ID of the tracing software's root span, so we can continue
+   *                            tracing the request as it flows through the relevant microservices.
+   *                            Will be set to a random ID if we're not logging a traceable request.
+   * @return Promise<void>
+   */
+  send( topic: string, message: Object, trace_id: string ): Promise<void>;
+
+}
