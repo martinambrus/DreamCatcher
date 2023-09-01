aff550e08a2027aa4feb226c25a904f60b15e483
Martin Ambruš <martin@martinambrus.com>
2023-09-01 14:48:56 +0200

    refactor: BullMQ to Kafka

--- workers/err_log_writer/app/MQx/MessageQueuePub.ts
+++ workers/err_log_writer/app/MQx/MessageQueuePub.ts
@@ -1,6 +1,7 @@
 import { IMessageQueuePub } from './KeyStore/Interfaces/IMessageQueuePub.js';
-import { Job, Queue } from 'bullmq';
-import { env } from "node:process";
+import { env, exit } from 'node:process';
+import { CompressionTypes, Kafka, Producer } from 'kafkajs';
+import { ILogger, LOG_SEVERITIES } from './KeyStore/Interfaces/ILogger.js';
 
 /**
  * Message queue class, responsible for sending messages
@@ -9,29 +10,57 @@ import { env } from "node:process";
 export class MessageQueuePub implements IMessageQueuePub {
 
   /**
-   * Server connection to use for this MQ instance.
+   * Client
+   * @type { Kafka }
    * @private
    */
-  private connection: any;
+  private client: Kafka;
 
   /**
-   * An object containing queues that exist on the MQ.
-   * These queues are named by topics of the MQ.
-   *
-   * @type { Object }
+   * Producer
+   * @type { any }
    * @private
    */
-  private queues: { [key: string]: Queue } = {};
+  private producer: Producer;
+
+  /**
+   * Determines whether the Kafka client was successfully connected.
+   * @type { boolean }
+   * @private
+   */
+  private ready: boolean = false;
+
+  /**
+   * A logger class instance.
+   * @type { ILogger }
+   * @private
+   */
+  private logger: ILogger;
 
   /**
    * Passes the connection and logger instances to this class.
    *
-   * @param { any }     connection Connection to the backend MQ server.
+   * @param { Kafka }  connection Connection to the backend MQ server.
+   * @param { ILogger} logger     Instance of the logging class.
    *
    * @constructor
    */
-  constructor( connection: any ) {
-    this.connection = connection;
+  constructor( connection: Kafka, logger: ILogger ) {
+    this.client = connection;
+    this.logger = logger;
+
+    // create a Kafka producer
+    this.producer = this.client.producer({
+      idempotent: true,
+    });
+
+    // connect to the producer
+    this.producer.connect().then( () => {
+      this.ready = true;
+    }).catch( ( err ) => {
+      console.log( this.logger.format( 'Exception while trying to connect to Kafka brokers (producer) ' + "\n" + JSON.stringify( err ) ) );
+      exit( 1 );
+    });
   }
 
   /**
@@ -41,47 +70,20 @@ export class MessageQueuePub implements IMessageQueuePub {
    * @param { Object } message       The actual message object. This will be JSON-encoded before sending.
    * @param { string } trace_id      Trace ID which we can use to continue tracing the lifetime of this request
    *                                 thorough all of the microservices ecosystem.
-   * @param { string } unique_job_id Optional parameters. If unique job ID is required, it can be passed here.
-   *
-   * @return Promise<Job> Returns a promise that resolves into a job added to the queue.
    */
-  public async send( topic: string, message: Object, trace_id: string, unique_job_id: string ): Promise<Job> {
-
-    // create a new Queue for this topic, if it does not exist yet
-    if ( !this.queues[ topic ] ) {
-      this.queues[ topic ] = new Queue( topic, {
-        connection: this.connection,
-        defaultJobOptions: {
-          attempts: parseInt( env.MQ_FAILED_JOBS_MAX_RETRIES ),
-          backoff: {
-            type: 'exponential',
-            delay: parseInt( env.MQ_FAILED_JOBS_BACKOFF_SECONDS ) * 1000
-          }
-        }
-      });
+  public async send( topic: string, message: Object, trace_id: string ): Promise<void> {
+    if ( this.ready ) {
+      try {
+        await this.producer.send({
+          topic: topic,
+          messages: [{ key: trace_id, value: JSON.stringify( message ) }],
+          acks: -1, // must be -1 because producer is set as idempotent, i.e. each message is written exactly once
+          compression: CompressionTypes.GZIP,
+        });
+      } catch ( err ) {
+        await this.logger.log_msg( 'Error publishing feed fetch to Kafka cluster:\n' + JSON.stringify( message ) + '\nerr: ' + JSON.stringify( err ), 'ERR_CONTROL_CENTER_CANNOT_PUBLISH_FEED', LOG_SEVERITIES.LOG_SEVERITY_ERROR, { trace_id: trace_id } );
+      }
     }
-
-    // send the message through the relevant queue
-    let opts = {
-      removeOnComplete: true,
-      removeOnFail: {
-        age: parseInt( env.MQ_FAILED_JOBS_REMOVAL_AGE_SECONDS ),
-      },
-    };
-
-    if ( unique_job_id ) {
-      opts[ 'jobId' ] = unique_job_id;
-    }
-
-    return this.queues[ topic ].add(
-      unique_job_id ?? trace_id,
-      {
-        msg: message,
-        trace_id: trace_id,
-      },
-      opts
-    );
-
   }
 
 }
