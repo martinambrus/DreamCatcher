f144cfc4e93f83e05c27ce8f78033c058fd79990
Martin Ambruš <martin@martinambrus.com>
2023-09-01 17:25:46 +0200

    Squash 1 commits
    * 968298a by Martin Ambruš <martin@martinambrus.com>
    Squash 1 commits

--- workers/link_fix_detector/app/MQx/KeyStore/Interfaces/IMessageQueueSub.ts
+++ workers/link_fix_detector/app/MQx/KeyStore/Interfaces/IMessageQueueSub.ts
@@ -7,11 +7,11 @@ export interface IMessageQueueSub {
    * Consumes messages from the given topic and passes them
    * to the callback function provided.
    *
-   * @param { string }   topic    The topic from which we want to be receiving messages with this class instance.
-   * @param { any }      callback The callback function to call when a new message arrives.
+   * @param { string|Array<string> } topic    The topic(s) from which we want to be receiving messages with this class instance.
+   * @param { any }                  callback The callback function to call when a new message arrives.
    * @return Promise<any>
    * @public
    */
-  consume( topic: string, callback: any ): Promise<any>;
+  consume( topic: string|Array<string>, callback: any ): Promise<any>;
 
 }
