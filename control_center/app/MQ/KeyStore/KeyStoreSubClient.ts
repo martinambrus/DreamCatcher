df6b1d9e21276fe959e8366ce94fc09e0677161c
Martin Ambruš <martin@martinambrus.com>
2023-09-02 10:31:55 +0200

    Squash 1 commits
    * e0d38ac by Martin Ambruš <martin@martinambrus.com>
    Squash 1 commits

--- control_center/app/MQ/KeyStore/KeyStoreSubClient.ts
+++ control_center/app/MQ/KeyStore/KeyStoreSubClient.ts
@@ -20,11 +20,27 @@ export class KeyStoreSubClient extends KeyStoreClientBase implements IKeyStoreSu
   /**
    * A proxy for KeyStore->subscribe()
    *
-   * @param { string }   channel  Name of the channel to subscribe to.
-   * @param { Function } callback Function to be executed when a new message from our channel arrives.
+   * @param { string|Array<string> } channel  Name of the channel to subscribe to.
+   * @param { Function }             callback Function to be executed when a new message from our channel arrives.
    */
-  public subscribe( channel: string, callback: any ) {
-    this.client.subscribe( channel, callback );
+  public subscribe( channel: string|Array<string>, callback: any ) {
+    if ( !( channel instanceof Array ) ) {
+      channel = [ channel ];
+    }
+
+    for ( let channel_name of channel ) {
+      this.client.subscribe( channel_name, ( err, count ) => {
+        if ( err ) {
+          throw err;
+        } else {
+          this.client.on( 'message', ( channel, message ) => {
+            if ( channel == channel_name ) {
+              callback( message );
+            }
+          });
+        }
+      });
+    }
   }
 
 }
