d9bd0e92204bd3bafc5457f70f203fd518aab88d
Martin Ambruš <martin@martinambrus.com>
2023-09-04 23:41:19 +0200

    fix: use prod env file for prod Docker Compose YAMLs

--- restart_docker-prod.bat
+++ restart_docker-prod.bat
@@ -1,2 +1,2 @@
-docker compose -f docker-compose-prod.yml down
-docker compose -f docker-compose-prod.yml up -d --build
+docker compose -f docker-compose-prod.yml --env-file .env.prod  down
+docker compose -f docker-compose-prod.yml --env-file .env.prod up -d --build
--- stop_docker-prod.bat
+++ stop_docker-prod.bat
@@ -1,1 +1,1 @@
-docker compose -f docker-compose-prod.yml down
+docker compose -f docker-compose-prod.yml --env-file .env.prod down
