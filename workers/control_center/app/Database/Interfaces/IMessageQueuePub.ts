44a1ee1cc8b9ca920698a3c17b42e7e1cb2bb8a6
Martin Ambruš <martin@martinambrus.com>
2023-09-04 19:41:20 +0200

    Squash 2 commits
    * f4c3869 by Martin Ambruš <martin@martinambrus.com>
    Squash 25 commits
    * 03e8d88 by Martin Ambrus <martinambrus@users.noreply.github.com>
    Initial commit

---
+++ workers/control_center/app/Database/Interfaces/IMessageQueuePub.ts
@@ -0,0 +1,20 @@
+/**
+ * Interface for a publishing message queue broker.
+ */
+export interface IMessageQueuePub {
+
+  /**
+   * Sends message to the message queue.
+   *
+   * @param { string } topic         Topic to sent the message to.
+   * @param { Object } msg           Message data to publish. Will be converted into a JSON string.
+   * @param { string } trace_id      ID of the tracing software's root span, so we can continue
+   *                                 tracing the request as it flows through the relevant microservices.
+   *                                 Will be set to a random ID if we're not logging a traceable request.
+   * @param { string } unique_job_id Optional parameters. If unique job ID is required, it can be passed here.
+   *
+   * @return Promise<any>
+   */
+  send( topic: string, message: Object, trace_id: string, unique_job_id?: string ): Promise<any>;
+
+}
